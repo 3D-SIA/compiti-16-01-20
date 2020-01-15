10220adf44f28ee76194f85ec2d8544006eb72a9
ArchonKlaus <42522736+ArchonKlaus@users.noreply.github.com>
2020-01-15 22:32:04 +0100

    Aggiunti esercizi 52 - 53

---
+++ es 52/es 52/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim scaglione As String
+        Dim trattenuta, stipendio As Double
+        stipendio = InputBox("Inserisci lo stipendio")
+        scaglione = InputBox("Inserisci numero scaglione")
+        If (scaglione = "1") Then
+            trattenuta = (stipendio * 5) / 100
+        Else
+            If (scaglione = "2") Then
+                trattenuta = (stipendio * 10) / 100
+            Else
+                If (scaglione = "3") Then
+                    trattenuta = (stipendio * 15) / 100
+                Else
+                    If (scaglione = "4") Then
+                        trattenuta = (stipendio * 25) / 100
+                    Else
+                        If (scaglione = "5") Then
+                            trattenuta = (stipendio * 35) / 100
+                        Else
+                            Return
+                        End If
+                    End If
+                End If
+            End If
+        End If
+        MsgBox("La trattenuta da versare è di " & trattenuta & "€")
+    End Sub
+End Class
---
+++ es 53/es 53/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pezzi, prezzo, sconto, totale As Double
+        prezzo = InputBox("Inserisci prezzo del prodotto")
+        pezzi = InputBox("Inserisci numero dei pezzi")
+        If (pezzi = 1) Then
+            sconto = (prezzo * 10) / 100
+            totale = prezzo - sconto
+        ElseIf (pezzi = 3) Then
+            sconto = (prezzo * 20) / 100
+            totale = prezzo - sconto
+        ElseIf (pezzi = 5) Then
+            sconto = (prezzo * 30) / 100
+            totale = prezzo - sconto
+        ElseIf (pezzi = 10) Then
+            sconto = (prezzo * 35) / 100
+            totale = prezzo - sconto
+        ElseIf (pezzi > 10) Then
+            sconto = (prezzo * 40) / 100
+            totale = prezzo - sconto
+        Else
+            MsgBox("Il totale è " & prezzo & "€")
+            Close()
+        End If
+        MsgBox("Il totale è " & totale & "€")
+        Close()
+    End Sub
+End Class
